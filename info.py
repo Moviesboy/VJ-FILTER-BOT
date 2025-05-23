8773c3cde637eb822513ad6790d4c90f7a8f5d81
Moviesboy <mrnobodytg026@gmail.com>
2025-05-23 18:57:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://wickm5055:<db_password>@nobodytg.pwguuvg.mongodb.net/?retryWrites=true&w=majority&appName=NobodyTG")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Filmy2zbot:<db_7trfiJy9cOfjDaTE>@cluster0.e7qj8ln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
